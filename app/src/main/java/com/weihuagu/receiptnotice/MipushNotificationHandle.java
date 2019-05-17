eba15c91cb846c31e3baed38d6c7aa1f16f326da
weihuagu <weihuagu_work@163.com>
2019-05-17 14:41:34 +0800

    因为使用小米推送，云闪付推送更加即时，增加com.xiaomi.xmsf下的云闪付推送,#21

--- app/src/main/java/com/weihuagu/receiptnotice/MipushNotificationHandle.java
+++ app/src/main/java/com/weihuagu/receiptnotice/MipushNotificationHandle.java
@@ -3,6 +3,9 @@ import android.app.Notification;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 
 public class MipushNotificationHandle extends NotificationHandle{
@@ -11,8 +14,8 @@ public class MipushNotificationHandle extends NotificationHandle{
         }
 
         public void handleNotification(){
-             if(title.contains("支付宝")){
-                if(content.contains("成功收款")){
+                if(title.contains("支付宝")){
+                        if(content.contains("成功收款")){
                                 Map<String,String> postmap=new HashMap<String,String>();
                                 postmap.put("type","alipay");
                                 postmap.put("time",notitime);
@@ -23,19 +26,37 @@ public class MipushNotificationHandle extends NotificationHandle{
                                 return ;
                         }
                 }
+                if(title.contains("动账通知")){
+                        if(content.contains("入账")&&content.contains("尾号为")){
+                                Map<String,String> postmap=new HashMap<String,String>();
+                                postmap.put("type","unionpay");
+                                postmap.put("time",notitime);
+                                postmap.put("title","云闪付扫码收款");
+                                postmap.put("money",extractMoney(content));
+                                postmap.put("content",content);
+                                //postmap.put("payer",getPayer(content));
+                                postpush.doPost(postmap);
+                                return ;
+                        }
+                }
+
+
 
 
         }
 
 
+        
+        private String getPayer(String content){
+                Pattern pattern = Pattern.compile("(.*)(通过扫码向您付款)");
+                Matcher matcher = pattern.matcher(content);
+                if(matcher.find()){
+                        String tmp=matcher.group(2);
+                        return tmp;
+                }else
+                        return "";
 
-
-
-
-
-
-
-
+        }
 
 
 
