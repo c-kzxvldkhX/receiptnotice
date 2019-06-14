627d66da483d0e2d540f77a2ab0cc82d120f7bdd
weihuagu <weihuagu_work@163.com>
2019-06-14 11:15:53 +0800

    由于支付宝转账码通知看不到金额，金额返回-0.00,并且加入转账人

--- app/src/main/java/com/weihuagu/receiptnotice/AlipayNotificationHandle.java
+++ app/src/main/java/com/weihuagu/receiptnotice/AlipayNotificationHandle.java
@@ -3,6 +3,8 @@ import android.app.Notification;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class AlipayNotificationHandle extends NotificationHandle{
@@ -23,13 +25,36 @@ public class AlipayNotificationHandle extends NotificationHandle{
                                 postpush.doPost(postmap);
                                 return ;
                         }
+                        if(content.contains("向你转了1笔钱")){
+                                Map<String,String> postmap=new HashMap<String,String>();
+                                postmap.put("type","alipay-transfer");
+                                postmap.put("time",notitime);
+                                postmap.put("title","转账");
+                                postmap.put("money","-0.00");
+                                postmap.put("content",content);
+                                postmap.put("transferor",whoTransferred(content));
+
+                                postpush.doPost(postmap);
+                                return ;
+                        }
                 }
 
 
 
         }
 
-
+        private String whoTransferred(String content){
+                Pattern pattern = Pattern.compile("(.*)(已成功向你转了)");
+                Matcher matcher = pattern.matcher(content);
+                if(matcher.find()){
+                    String tmp=matcher.group(1);
+					return tmp;
+                
+                }
+                else
+                    return "";  
+        
+        }
 
 
 
--- app/src/main/java/com/weihuagu/receiptnotice/MipushNotificationHandle.java
+++ app/src/main/java/com/weihuagu/receiptnotice/MipushNotificationHandle.java
@@ -25,6 +25,18 @@ public class MipushNotificationHandle extends NotificationHandle{
                                 postpush.doPost(postmap);
                                 return ;
                         }
+                        if(content.contains("向你转了1笔钱")){
+                                Map<String,String> postmap=new HashMap<String,String>();
+                                postmap.put("type","alipay-transfer");
+                                postmap.put("time",notitime);
+                                postmap.put("title","转账");
+                                postmap.put("money","-0.00");
+                                postmap.put("content",content);
+                                postmap.put("transferor",whoTransferred(content));
+
+                                postpush.doPost(postmap);
+                                return ;
+                        }
                 }
                 if(title.contains("动账通知")){
                         if(content.contains("入账")&&content.contains("尾号为")){
@@ -58,6 +70,18 @@ public class MipushNotificationHandle extends NotificationHandle{
 
         }
 
+        private String whoTransferred(String content){
+                Pattern pattern = Pattern.compile("(.*)(已成功向你转了)");
+                Matcher matcher = pattern.matcher(content);
+                if(matcher.find()){
+                    String tmp=matcher.group(1);
+					return tmp;
+                
+                }
+                else
+                    return "";  
+        
+        }
 
 
 }
