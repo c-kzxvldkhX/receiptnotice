805afba1b31307755a5e2ec008f5dbd545a0d69c
weihuagu <weihuagu_work@163.com>
2019-09-07 21:44:10 +0800

    推送地址输入框增加下拉提示,#43

---
+++ app/src/main/java/com/weihuagu/receiptnotice/BanksProxy.java
@@ -0,0 +1,32 @@
+package com.weihuagu.receiptnotice;
+import android.app.Notification;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class BanksProxy extends NotificationHandle{
+        public BanksProxy(String pkgtype,Notification notification,IDoPost postpush){
+                super(pkgtype,notification,postpush);
+        }
+
+        public void handleNotification(){
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+}
