49866d98683c6d58efa5b18c1197a868a138fad4
weihuagu <weihuagu_work@163.com>
2019-09-10 22:45:30 +0800

    继续写银行分类

--- app/src/main/java/com/weihuagu/receiptnotice/BanksProxy.java
+++ app/src/main/java/com/weihuagu/receiptnotice/BanksProxy.java
@@ -11,8 +11,36 @@ public class BanksProxy extends NotificationHandle{
         public BanksProxy(String pkgtype,Notification notification,IDoPost postpush){
                 super(pkgtype,notification,postpush);
         }
+        
+        private String getBankType(){
+                BankDistinguisher onedistinguisher=new BankDistinguisher();
+                return onedistinguisher.distinguishByMessageContent(content);
+        }
+        private  String extractMoney(String content){
+                Pattern pattern = Pattern.compile("(收入|存入|转入|入账)(([1-9]{1}\\d*)|([0]{1}))(\\.(\\d){0,2})?元");
+                Matcher matcher = pattern.matcher(content);
+                if(matcher.find()){
+                        String tmp=matcher.group();
+                        Pattern patternnum = Pattern.compile("(([1-9]{1}\\d*)|([0]{1}))(\\.(\\d){0,2})?");
+                        Matcher matchernum = patternnum.matcher(tmp);
+                        if(matchernum.find())
+                                return matchernum.group();
+                        return null;
+                }else
+                        return null;
+
+
+        }
+
 
         public void handleNotification(){
+                String banktype=getBankType();
+                String type=null;
+                if(banktype!=null)
+                        if(banktype=="")
+                                ;
+                        else
+                                ;
 
 
 
