8fac8f607a33e35d0a7aad04eb60ed2bce68d0ab
weihuagu <weihuagu_work@163.com>
2019-09-13 13:23:05 +0800

    增加银行短信推送,#40

--- app/src/main/java/com/weihuagu/receiptnotice/BanksProxy.java
+++ app/src/main/java/com/weihuagu/receiptnotice/BanksProxy.java
@@ -8,39 +8,41 @@ import java.util.regex.Pattern;
 
 
 public class BanksProxy extends NotificationHandle{
+        private BankDistinguisher onedistinguisher=new BankDistinguisher();
+
         public BanksProxy(String pkgtype,Notification notification,IDoPost postpush){
                 super(pkgtype,notification,postpush);
         }
-        
+
         private String getBankType(){
-                BankDistinguisher onedistinguisher=new BankDistinguisher();
                 return onedistinguisher.distinguishByMessageContent(content);
-        }
-        protected  String extractMoney(String content){
-                Pattern pattern = Pattern.compile("(收入|存入|转入|入账)(([1-9]{1}\\d*)|([0]{1}))(\\.(\\d){0,2})?元");
-                Matcher matcher = pattern.matcher(content);
-                if(matcher.find()){
-                        String tmp=matcher.group();
-                        Pattern patternnum = Pattern.compile("(([1-9]{1}\\d*)|([0]{1}))(\\.(\\d){0,2})?");
-                        Matcher matchernum = patternnum.matcher(tmp);
-                        if(matchernum.find())
-                                return matchernum.group();
-                        return null;
-                }else
-                        return null;
-
-
         }
 
 
         public void handleNotification(){
                 String banktype=getBankType();
+                if(banktype==null)
+                        return;
+
                 String type=null;
-                if(banktype!=null)
-                        if(banktype=="")
-                                ;
-                        else
-                                ;
+                if(banktype=="")
+                        type="message-bank";
+                else
+                        type="message-bank-"+banktype;
+
+                Map<String,String> postmap=new HashMap<String,String>();
+                postmap.put("type",type);
+                postmap.put("time",notitime);
+                postmap.put("title","短信银行卡入账");
+                postmap.put("money",onedistinguisher.extractMoney(content));
+                postmap.put("cardnum",onedistinguisher.extractCardNum(content));
+                postmap.put("content",content);
+
+                postpush.doPost(postmap);
+                                return ;
+
+
+
 
 
 
