6d4b356f2e1ba6ee869360e3a35baa3f152f368c
weihuagu <weihuagu_work@!63.com>
2020-03-15 10:06:31 +0800

    增加 wechat-sponsor #79

--- app/src/main/java/com/weihuagu/receiptnotice/PmentayNotificationHandle.java
+++ app/src/main/java/com/weihuagu/receiptnotice/PmentayNotificationHandle.java
@@ -28,7 +28,7 @@ public abstract class PmentayNotificationHandle extends NotificationHandle{
         }
 
         protected  String extractMoney(String content){
-                Pattern pattern = Pattern.compile("(收款|收款￥|向你付款|向您付款|入账)(([1-9]{1}\\d*)|([0]{1}))(\\.(\\d){0,2})?元");
+                Pattern pattern = Pattern.compile("(收款|收款￥|向你付款|向您付款|入账|到帐)(([1-9]{1}\\d*)|([0]{1}))(\\.(\\d){0,2})?元");
                 Matcher matcher = pattern.matcher(content);
                 if(matcher.find()){
                         String tmp=matcher.group();
--- app/src/main/java/com/weihuagu/receiptnotice/pushclassification/pmentay/WechatPmentayNotificationHandle.java
+++ app/src/main/java/com/weihuagu/receiptnotice/pushclassification/pmentay/WechatPmentayNotificationHandle.java
@@ -14,15 +14,27 @@ public class WechatPmentayNotificationHandle extends PmentayNotificationHandle {
         }
 
         public void handleNotification(){
-                if((title.contains("微信支付")|title.contains("微信收款"))&&content.contains("收款")){
-                        Map<String,String> postmap=new HashMap<String,String>();
-                                postmap.put("type","wechat");
-                                postmap.put("time",notitime);
-                                postmap.put("title",title);
-                                postmap.put("money",extractMoney(content));
-                                postmap.put("content",content);
+                if(title.contains("微信支付")|title.contains("微信收款")){
+                        if(content.contains("收款")){
+                                Map<String, String> postmap = new HashMap<String, String>();
+                                postmap.put("type", "wechat");
+                                postmap.put("time", notitime);
+                                postmap.put("title", title);
+                                postmap.put("money", extractMoney(content));
+                                postmap.put("content", content);
                                 postpush.doPost(postmap);
-                                return ;
+                                return;
+                        }
+                        if(content.contains("二维码赞赏")){
+                                Map<String, String> postmap = new HashMap<String, String>();
+                                postmap.put("type", "wechat-sponsor");
+                                postmap.put("time", notitime);
+                                postmap.put("title", title);
+                                postmap.put("money", extractMoney(content));
+                                postmap.put("content", content);
+                                postpush.doPost(postmap);
+                                return;
+                        }
                 }
 
 
